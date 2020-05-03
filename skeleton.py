4c6cd9d70cdfb2984b037726dba464f88a09d5c5
Kamilla Aslami <kamilla.1201@gmail.com>
2020-05-03 15:06:22 -0700

    upd

---
+++ skeleton.py
@@ -0,0 +1,33 @@
+questions = [
+    "what are the advantages of rust over c++?",
+    "what is the distance between Omaha and Portland?",
+    "negative index in python",
+    "When is Thanksgiving Day 2020?",
+    "what is yml?",
+    "10 gallons in liters"
+]
+
+def get_answer_from_qa_api(question):
+    pass
+
+def get_answer_from_qa_api(question):
+    pass
+
+
+for question in questions:
+    print("----------------------------------")
+    print("Getting results from Quick Answers API")
+    print("----------------------------------")
+    print("Question: " + question)
+    print("----------------------------------")
+    print(get_answer_from_qa_api(question))
+    print("----------------------------------")
+
+for question in questions:
+    print("----------------------------------")
+    print("Getting results from Quick Answers API")
+    print("----------------------------------")
+    print("Question: " + question)
+    print("----------------------------------")
+    print(get_answer_from_ms_api(question))
+    print("----------------------------------")
